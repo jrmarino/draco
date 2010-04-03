642d9bc55609e429f6ce616d694e96b65b1f1a16
John Marino <aux@marino.st>
2010-04-03 13:40:29 -0500

    Adding AMD64 support for freeBSD and Dragonfly Unwind
    I found a patch from David Xu that provided the missing x86_64 unwind
    handing for the MD_FALLBACK_FRAME_STATE_FOR macro.  This is completely
    untested and strangely Xu's patch did not get any attention.  It did not
    make it into GCC.  No promises, let's see how it performs.

--- trunk/gcc/config/i386/freebsd-unwind.h
+++ trunk/gcc/config/i386/freebsd-unwind.h
@@ -1,6 +1,5 @@
-/* DWARF2 EH unwinding support for x86-freebsd
-   Copyright (C) 2004, 2005, 2006, 2007, 2008
-   Free Software Foundation, Inc.
+/* DWARF2 EH unwinding support for FreeBSD: AMD x86-64 and x86.
+   Copyright (C) 2004, 2005, 2006 Free Software Foundation, Inc.
    Copyright (C) 2010 AuroraUX (www.auroraux.org)
 
 This file is part of GCC.
@@ -31,8 +30,97 @@ Boston, MA 02110-1301, USA.  */
 /* Do code reading to identify a signal frame, and set the frame
    state data appropriately.  See unwind-dw2.c for the structs. */
 
+#include <sys/types.h>
+#include <sys/sysctl.h>
 #include <signal.h>
 #include <sys/ucontext.h>
+#include <machine/sigframe.h>
+
+
+#define REG_NAME(reg)	sf_uc.uc_mcontext.mc_## reg
+
+static void
+x86_sigtramp_range (unsigned char **start, unsigned char **end)
+{
+  static unsigned long ps_strings = 0;
+  int mib[2];
+  size_t len;
+
+  if (ps_strings == 0)
+    {
+     mib[0] = CTL_KERN;
+     mib[1] = KERN_PS_STRINGS;
+     len = sizeof (ps_strings);
+     sysctl (mib, 2, &ps_strings, &len, NULL, 0);
+    }
+
+  *start = (unsigned char *)ps_strings - 128;
+  *end = (unsigned char *)ps_strings;
+}
+
+#ifdef __x86_64__
+
+#define MD_FALLBACK_FRAME_STATE_FOR x86_64_freebsd_fallback_frame_state
+
+static _Unwind_Reason_Code
+x86_64_freebsd_fallback_frame_state
+(struct _Unwind_Context *context, _Unwind_FrameState *fs)
+{
+  unsigned char *pc = context->ra;
+  unsigned char *sigtramp_start, *sigtramp_end;
+  struct sigframe *sf;
+  long new_cfa;
+
+  x86_sigtramp_range(&sigtramp_start, &sigtramp_end);
+  if (pc >= sigtramp_end || pc < sigtramp_start)
+    return _URC_END_OF_STACK;
+
+  sf = (struct sigframe *) context->cfa;
+  new_cfa = sf->REG_NAME(rsp);
+  fs->cfa_how = CFA_REG_OFFSET;
+  /* Register 7 is rsp  */
+  fs->cfa_reg = 7;
+  fs->cfa_offset = new_cfa - (long) context->cfa;
+
+  /* The SVR4 register numbering macros aren't usable in libgcc.  */
+  fs->regs.reg[0].how = REG_SAVED_OFFSET;
+  fs->regs.reg[0].loc.offset = (long)&sf->REG_NAME(rax) - new_cfa;
+  fs->regs.reg[1].how = REG_SAVED_OFFSET;
+  fs->regs.reg[1].loc.offset = (long)&sf->REG_NAME(rdx) - new_cfa;
+  fs->regs.reg[2].how = REG_SAVED_OFFSET;
+  fs->regs.reg[2].loc.offset = (long)&sf->REG_NAME(rcx) - new_cfa;
+  fs->regs.reg[3].how = REG_SAVED_OFFSET;
+  fs->regs.reg[3].loc.offset = (long)&sf->REG_NAME(rbx) - new_cfa;
+  fs->regs.reg[4].how = REG_SAVED_OFFSET;
+  fs->regs.reg[4].loc.offset = (long)&sf->REG_NAME(rsi) - new_cfa;
+  fs->regs.reg[5].how = REG_SAVED_OFFSET;
+  fs->regs.reg[5].loc.offset = (long)&sf->REG_NAME(rdi) - new_cfa;
+  fs->regs.reg[6].how = REG_SAVED_OFFSET;
+  fs->regs.reg[6].loc.offset = (long)&sf->REG_NAME(rbp) - new_cfa;
+  fs->regs.reg[8].how = REG_SAVED_OFFSET;
+  fs->regs.reg[8].loc.offset = (long)&sf->REG_NAME(r8) - new_cfa;
+  fs->regs.reg[9].how = REG_SAVED_OFFSET;
+  fs->regs.reg[9].loc.offset = (long)&sf->REG_NAME(r9) - new_cfa;
+  fs->regs.reg[10].how = REG_SAVED_OFFSET;
+  fs->regs.reg[10].loc.offset = (long)&sf->REG_NAME(r10) - new_cfa;
+  fs->regs.reg[11].how = REG_SAVED_OFFSET;
+  fs->regs.reg[11].loc.offset = (long)&sf->REG_NAME(r11) - new_cfa;
+  fs->regs.reg[12].how = REG_SAVED_OFFSET;
+  fs->regs.reg[12].loc.offset = (long)&sf->REG_NAME(r12) - new_cfa;
+  fs->regs.reg[13].how = REG_SAVED_OFFSET;
+  fs->regs.reg[13].loc.offset = (long)&sf->REG_NAME(r13) - new_cfa;
+  fs->regs.reg[14].how = REG_SAVED_OFFSET;
+  fs->regs.reg[14].loc.offset = (long)&sf->REG_NAME(r14) - new_cfa;
+  fs->regs.reg[15].how = REG_SAVED_OFFSET;
+  fs->regs.reg[15].loc.offset = (long)&sf->REG_NAME(r15) - new_cfa;
+  fs->regs.reg[16].how = REG_SAVED_OFFSET;
+  fs->regs.reg[16].loc.offset = (long)&sf->REG_NAME(rip) - new_cfa;
+  fs->retaddr_column = 16;
+  fs->signal_frame = 1;
+  return _URC_NO_REASON;
+}
+
+#else /* Next section is for i386  */
 
 #define MD_FALLBACK_FRAME_STATE_FOR x86_freebsd_fallback_frame_state
 
@@ -40,65 +128,41 @@ static _Unwind_Reason_Code
 x86_freebsd_fallback_frame_state
 (struct _Unwind_Context *context, _Unwind_FrameState *fs)
 {
-    unsigned char *pc_ = context->ra;
-    struct sigcontext *sc_;
-    long new_cfa_;
+  unsigned char *pc = context->ra;
+  unsigned char *sigtramp_start, *sigtramp_end;
+  struct sigframe *sf;
+  long new_cfa;
 
-    /* Standard sigcode */
-    /*  movl $SYS_sigreturn, %eax; pushl %eax; int $0x80 */
-    if (*(unsigned int *)(pc_+17) == 0x0001a1b8
-	&& *(unsigned char *)(pc_+22) == 0x50
-	&& *(unsigned short *)(pc_+23) == 0x80cd) {
-      sc_ = context->cfa + 32;
+  x86_sigtramp_range(&sigtramp_start, &sigtramp_end);
 
-    /* libpthread _thr_sig_handler */
-    /* if ((sa_flags & SA_SIGINFO) != 0 || ... )
-                        (*(sigfunc))(sig, info, ucp); */
+  if (pc >= sigtramp_end || pc < sigtramp_start)
+    return _URC_END_OF_STACK;
 
-    } else if (*(unsigned int *)(pc_-27) == 0xff6885f6
-      && *(unsigned int *)(pc_-23) == 0x7540ffff
-      && *(unsigned short *)(pc_) == 0xc483
-      && *(unsigned char *)(pc_+2) == 0x10) {
-      sc_ = context->cfa + 224;
+  sf = (struct sigframe *) context->cfa;
+  new_cfa = sf->REG_NAME(esp);
+  fs->cfa_how = CFA_REG_OFFSET;
+  fs->cfa_reg = 4;
+  fs->cfa_offset = new_cfa - (long) context->cfa;
 
-    /* libpthread handle_signal */
-    /* if ((shi->sa_flags & SA_SIGINFO) != 0 || ... )
-                        (*(shi->sigfunc))(shi->sig, shi->info, shi->ucp); */
-
-    } else if (*(unsigned int *)(pc_-26) == 0x400446f6
-      && *(unsigned short *)(pc_-2) == 0x16ff
-      && *(unsigned short *)(pc_) == 0xc483
-      && *(unsigned char *)(pc_+2) == 0x10) {
-      sc_ = context->cfa + 544;
-
-    /* No match */
-    } else {
-      return _URC_END_OF_STACK;
-    }
-
-    new_cfa_ = sc_->sc_esp;
-    fs->regs.cfa_how = CFA_REG_OFFSET;
-    fs->regs.cfa_reg = __builtin_dwarf_sp_column ();
-    fs->regs.cfa_offset = new_cfa_ - (long) context->cfa;
-
-    /* The SVR4 register numbering macros aren't usable in libgcc.  */
-    fs->regs.reg[0].how = REG_SAVED_OFFSET;
-    fs->regs.reg[0].loc.offset = (long)&sc_->sc_eax - new_cfa_;
-    fs->regs.reg[3].how = REG_SAVED_OFFSET;
-    fs->regs.reg[3].loc.offset = (long)&sc_->sc_ebx - new_cfa_;
-    fs->regs.reg[1].how = REG_SAVED_OFFSET;
-    fs->regs.reg[1].loc.offset = (long)&sc_->sc_ecx - new_cfa_;
-    fs->regs.reg[2].how = REG_SAVED_OFFSET;
-    fs->regs.reg[2].loc.offset = (long)&sc_->sc_edx - new_cfa_;
-    fs->regs.reg[6].how = REG_SAVED_OFFSET;
-    fs->regs.reg[6].loc.offset = (long)&sc_->sc_esi - new_cfa_;
-    fs->regs.reg[7].how = REG_SAVED_OFFSET;
-    fs->regs.reg[7].loc.offset = (long)&sc_->sc_edi - new_cfa_;
-    fs->regs.reg[5].how = REG_SAVED_OFFSET;
-    fs->regs.reg[5].loc.offset = (long)&sc_->sc_ebp - new_cfa_;
-    fs->regs.reg[8].how = REG_SAVED_OFFSET;
-    fs->regs.reg[8].loc.offset = (long)&sc_->sc_eip - new_cfa_;
-    fs->retaddr_column = 8;
-    fs->signal_frame = 1;
-    return _URC_NO_REASON;
+  /* The SVR4 register numbering macros aren't usable in libgcc.  */
+  fs->regs.reg[0].how = REG_SAVED_OFFSET;
+  fs->regs.reg[0].loc.offset = (long)&sf->REG_NAME(eax) - new_cfa;
+  fs->regs.reg[3].how = REG_SAVED_OFFSET;
+  fs->regs.reg[3].loc.offset = (long)&sf->REG_NAME(ebx) - new_cfa;
+  fs->regs.reg[1].how = REG_SAVED_OFFSET;
+  fs->regs.reg[1].loc.offset = (long)&sf->REG_NAME(ecx) - new_cfa;
+  fs->regs.reg[2].how = REG_SAVED_OFFSET;
+  fs->regs.reg[2].loc.offset = (long)&sf->REG_NAME(edx) - new_cfa;
+  fs->regs.reg[6].how = REG_SAVED_OFFSET;
+  fs->regs.reg[6].loc.offset = (long)&sf->REG_NAME(esi) - new_cfa;
+  fs->regs.reg[7].how = REG_SAVED_OFFSET;
+  fs->regs.reg[7].loc.offset = (long)&sf->REG_NAME(edi) - new_cfa;
+  fs->regs.reg[5].how = REG_SAVED_OFFSET;
+  fs->regs.reg[5].loc.offset = (long)&sf->REG_NAME(ebp) - new_cfa;
+  fs->regs.reg[8].how = REG_SAVED_OFFSET;
+  fs->regs.reg[8].loc.offset = (long)&sf->REG_NAME(eip) - new_cfa;
+  fs->retaddr_column = 8;
+  fs->signal_frame = 1;
+  return _URC_NO_REASON;
 }
+#endif /* ifdef __x86_64__  */
