5e8eb46979be599ba69888dc217d70a9a34c9c89
John Marino <aux@marino.st>
2010-04-21 15:33:34 -0500

    Ada "Pathinfo" package
    It seems that Ada didn't have a PHP-style set of pathinfo functions, so I
    created my own, which served to illustrate how much I don't know about Ada
    string handling!

---
+++ draco_trunk/driver/pathinfo.adb
@@ -0,0 +1,184 @@
+--
+--  DRACO ADA COMPILER
+--  DRIVER COMPONENT
+--
+--
+--  Copyright (c) 2010, AuroraUX (www.auroraux.org)
+--  All rights reserved.
+--
+--  Permission to use, copy, modify, and/or distribute this software for any
+--  purpose with or without fee is hereby granted, provided that the above
+--  copyright notice and this permission notice appear in all copies.
+--
+--  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+--  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+--  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+--  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+--  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+--  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+--  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+package body PathInfo is
+
+
+   --------------
+   --  Dirname --
+   --------------
+
+   function Dirname (path : in SU.Unbounded_String) return
+      SU.Unbounded_String is
+      result : SU.Unbounded_String;
+      index : Natural;
+   begin
+      index := SU.Index (
+          source  => path,
+          Pattern => Slash,
+          Going   => Ada.Strings.Backward
+      );
+      case index is
+         when 0      => result := USDot;
+         when 1      => result := SU.To_Unbounded_String (Slash);
+         when others => result := SU.Unbounded_Slice (Path, 1, index - 1);
+      end case;
+
+      return result;
+   end Dirname;
+
+
+   ---------------
+   --  Basename --
+   ---------------
+
+   function Basename  (path : in SU.Unbounded_String) return
+      SU.Unbounded_String is
+      full_filename : SU.Unbounded_String;
+      result : SU.Unbounded_String;
+      index  : Natural;
+   begin
+      full_filename := Filename (path);
+      index := SU.Index (
+          source  => full_filename,
+          Pattern => SDot,
+          Going   => Ada.Strings.Backward
+      );
+      if index <= 1 then 
+         result := full_filename;
+      else 
+         result := SU.Unbounded_Slice (full_filename, 1, index - 1);
+      end if;
+
+      return result;
+   end Basename;
+
+
+
+   -----------------
+   --  Extension  --
+   -----------------
+
+   function Extension (path   : in SU.Unbounded_String) return
+      SU.Unbounded_String is
+      full_filename : SU.Unbounded_String;
+      result  : SU.Unbounded_String;
+      namelen : Natural;
+      index   : Natural;
+   begin  
+      full_filename := Filename (path);
+      namelen       := SU.Length (full_filename);
+      index := SU.Index (
+          source  => full_filename,
+          Pattern => SDot,
+          Going   => Ada.Strings.Backward
+      );
+      if index <= 1 then 
+         result := SU.Null_Unbounded_String;
+      elsif index = namelen then
+         result := SU.Null_Unbounded_String;
+      else
+         result := SU.Unbounded_Slice 
+                  (full_filename, index + 1 , namelen);
+      end if;
+
+      return result;
+   end Extension;
+
+
+   ----------------
+   --  Filename  --
+   ----------------
+
+   function Filename  (path   : in SU.Unbounded_String) return
+      SU.Unbounded_String is
+      result : SU.Unbounded_String;
+      index : Natural;
+      pathlen : Natural;
+   begin
+      pathlen := SU.Length (path);
+      index := SU.Index (
+          source  => path,
+          Pattern => Slash,
+          Going   => Ada.Strings.Backward
+      );
+      if index = 0 then 
+         result := path;
+      elsif index = pathlen then
+         result := SU.Null_Unbounded_String;
+      else
+         result := SU.Unbounded_Slice (path, index + 1,  pathlen);
+      end if;
+
+      return result;
+   end Filename;
+
+
+   function Info (path   : in SU.Unbounded_String) return recPathInfo is
+      result  : RecPathInfo;
+      pathlen : Natural;
+      namelen : Natural;
+      index   : Natural;
+   begin
+      pathlen := SU.Length (path);
+      index := SU.Index (
+          source  => path,
+          Pattern => Slash,
+          Going   => Ada.Strings.Backward
+      );
+      case index is
+         when 0      => result.dirname := USDot;
+         when 1      => result.dirname := SU.To_Unbounded_String (Slash);
+         when others => result.dirname := SU.Unbounded_Slice 
+                                          (path, 1, index - 1);
+      end case;
+
+      if index = 0 then 
+         result.filename := path;
+      elsif index = pathlen then
+         result.filename := SU.Null_Unbounded_String;
+      else 
+         result.filename := SU.Unbounded_Slice (path, index + 1,  pathlen);
+      end if;
+
+      index := SU.Index (
+          source  => result.filename,
+          Pattern => SDot,
+          Going   => Ada.Strings.Backward
+      );
+      namelen := SU.Length (result.filename);
+      if (index <= 1) or (index = namelen) then
+            result.extension := SU.Null_Unbounded_String;
+      else
+            result.extension := SU.Unbounded_Slice (result.filename, 
+                                index + 1, namelen);
+      end if;
+
+      if index <= 1 then 
+         result.basename := result.filename;
+      else 
+         result.basename := SU.Unbounded_Slice (result.filename, 1, index - 1);
+      end if;
+
+      return result;
+   end Info;
+
+end PathInfo;
---
+++ draco_trunk/driver/pathinfo.ads
@@ -0,0 +1,81 @@
+--
+--  DRACO ADA COMPILER
+--  DRIVER COMPONENT
+--
+--
+--  Copyright (c) 2010, AuroraUX (www.auroraux.org)
+--  All rights reserved.
+--
+--  Permission to use, copy, modify, and/or distribute this software for any
+--  purpose with or without fee is hereby granted, provided that the above
+--  copyright notice and this permission notice appear in all copies.
+--
+--  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+--  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+--  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+--  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+--  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+--  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+--  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+with GNAT.OS_Lib;
+with Ada.Strings.Unbounded;
+
+package PathInfo is
+
+   package SU renames Ada.Strings.Unbounded;
+
+   SDot  : constant String := ".";
+   USDot : constant SU.Unbounded_String := SU.To_Unbounded_String (SDot);
+   Slash : constant String := String'(1 => GNAT.OS_Lib.Directory_Separator);
+
+
+   type RecPathInfo is record
+      dirname   : SU.Unbounded_String;
+      basename  : SU.Unbounded_String;
+      filename  : SU.Unbounded_String;
+      extension : SU.Unbounded_String;
+   end record;
+
+
+   function Dirname   (path   : in SU.Unbounded_String) return
+      SU.Unbounded_String;
+   --  This function will accept a file or directory path as its sole argument
+   --  and it will return the directory portion of the path.  If the path
+   --  given consists solely of a filename (e.g. no directory separators)
+   --  then the function will return ".".  The result will not include
+   --  trailing separators, except in the case of the root folder.
+
+
+   function Basename  (path   : in SU.Unbounded_String) return
+      SU.Unbounded_String;
+   --  This function will accept a path to a file, and return the
+   --  filename component of the path.  The result will trim the last period
+   --  and any following characters if it exists.  This is normally defined as
+   --  a file extension.  If this behavior is undesired, use the "filename"
+   --  function instead.
+
+
+   function Extension (path   : in SU.Unbounded_String) return
+      SU.Unbounded_String;
+   --  This function will accept a path to a file, and return just the
+   --  file extension, if it exists.  A file extension is defined as the final
+   --  period within a filename, and all characters that follow it.  If no
+   --  period exists, a null string will be returned.
+
+
+   function Filename  (path   : in SU.Unbounded_String) return
+      SU.Unbounded_String;
+   --  The function will accept a path to a file.  Internally it will strip
+   --  out the path to the directory, if one was provided, and it will return
+   --  just the filename without any directory separators.
+
+
+   function Info  (path   : in SU.Unbounded_String) return recPathInfo;
+   --  This function will accept a path to a file or directory, and it will
+   --  split this information up into various components: path to directory,
+   --  full filename, base filename, and filename extension.
+
+
+end PathInfo;
