3074cdd0ccfb41f9428ed3a690bee50282f61e33
John Marino <aux@marino.st>
2010-11-09 15:34:41 -0600

    Bad data cost me some time
    I spent a lot of time browsing the NetBSD source via cvsweb and it gave me
    bad information.  I don't know where I got the 64 bit integer for both
    systems when the truth was really 32-bit integer for both systems.
    That wasted two build times and the unecessary creation of a new
    OS primative file (since deleted).

--- trunk/gcc/ada/s-osinte-netbsd.ads
+++ trunk/gcc/ada/s-osinte-netbsd.ads
@@ -61,7 +61,6 @@ package System.OS_Interface is
    subtype unsigned_char  is Interfaces.C.unsigned_char;
    subtype plain_char     is Interfaces.C.plain_char;
    subtype size_t         is Interfaces.C.size_t;
-   subtype int64_t        is Interfaces.Integer_64;
 
    -----------
    -- Errno --
@@ -631,7 +630,7 @@ private
 
    type pid_t is new int;
 
-   type time_t is new int64_t;
+   type time_t is new int;
 
    type timespec is record
       tv_sec  : time_t;
