ff7595bb1f42b11a845fe62ff761b4cb0c12b075
John Marino <aux@marino.st>
2010-11-19 19:27:48 -0600

    Reversed AI-1057 reversal patch
    Looks like I have rebuilt gprbuild so that this patch is not needed.

--- trunk/gcc/ada/sem_intr.adb
+++ trunk/gcc/ada/sem_intr.adb
@@ -174,11 +174,7 @@ package body Sem_Intr is
       elsif Cnam = Name_Unchecked_Deallocation
         and then No_Pool_Assigned (Rtyp)
       then
-      --  AI-0157 implementation reversed until further notice
-      --  gprbuild 2010 can't be built with it present, and attempts to build
-      --  gprbuild with "gnat mode" were not successful. 
-      --  Error_Msg_N ("deallocation from empty storage pool!", N);
-         null;
+         Error_Msg_N ("deallocation from empty storage pool!", N);
 
       --  For now, no other special checks are required
 
