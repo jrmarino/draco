7ad3767d63bf643943fbcd631f7b35ce8462457a
John Marino <aux@marino.st>
2010-12-05 03:05:57 -0600

    Added GPS as a pkgsrc package
    See title.  It builds but didn't launch on dragonfly after the initial
    configuration file creation.  Don't know why yet.

---
+++ misc/package_source/pkgsrc/devel/gps/options.mk
@@ -0,0 +1,27 @@
+# $NetBSD$
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.gps
+PKG_SUPPORTED_OPTIONS=	sqlite pgsql
+PKG_SUGGESTED_OPTIONS=
+
+.include "../../mk/bsd.options.mk"
+
+
+######################
+##  SQLITE SUPPORT  ##
+######################
+
+.if !empty(PKG_OPTIONS:Msqlite)
+CONFIGURE_ARGS+= --with-sqlite=${PREFIX}
+.include "../../databases/sqlite/buildlink3.mk"
+.endif
+
+
+##########################
+##  POSTGRESQL SUPPORT  ##
+##########################
+
+.if !empty(PKG_OPTIONS:Mpgsql)
+CONFIGURE_ARGS+= --with-postgresql=${PREFIX}
+.include "../../databases/postgresql90-client/buildlink3.mk"
+.endif
