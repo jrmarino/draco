8d2a848a2d9a5600fe5556ba08bd184ef4a5c057
John Marino <aux@marino.st>
2010-12-30 13:14:59 -0600

    Removed redefinitions from Dragonfly64 fragment
    After the config.gcc entry for Dragonfly 64 was updated, the lined deleted
    from dragonfly64.h were made redundant.  This commit should have no effect,
    and it's considered a clean-up only.

--- trunk/gcc/config/i386/dragonfly64.h
+++ trunk/gcc/config/i386/dragonfly64.h
@@ -3,11 +3,3 @@
 #undef  TARGET_VERSION
 #define TARGET_VERSION fprintf (stderr, " (x86-64 DragonFly/ELF)");
 
-/* Tell final.c that we don't need a label passed to mcount.  */
-
-#undef  MCOUNT_NAME
-#define MCOUNT_NAME ".mcount"
-
-#undef	SUBTARGET_EXTRA_SPECS
-#define SUBTARGET_EXTRA_SPECS \
-  { "dfbsd_dynamic_linker", DFBSD_DYNAMIC_LINKER }
