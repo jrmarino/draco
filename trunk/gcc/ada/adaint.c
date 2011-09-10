2735dc411400016e2678f66442ffbc6cc0155ba9
John Marino <aux@marino.st>
2011-09-10 05:17:16 -0500

    Tested implementation of symbolic traceback support
    The first version of the traceback support had some issues, but this
    version works great and passes a basic test from rosetta stone.

--- trunk/gcc/ada/adaint.c
+++ trunk/gcc/ada/adaint.c
@@ -1203,8 +1203,8 @@ __gnat_tmp_name (char *tmp_filename)
 
 #elif defined (__ANDROID__)
 
-  /* 
-   * ext2 /ext3/ext4/fat16/fat32 have no path limits 
+  /*
+   * ext2 /ext3/ext4/fat16/fat32 have no path limits
    * /data/local/tmp normally requires rooted devices, if it even exists
    * /sdcard is the standard location for external storage.  Nativeactivity
    * manifest needs to authorize its use, otherwise it might not have the
@@ -1216,10 +1216,10 @@ __gnat_tmp_name (char *tmp_filename)
 
   if (datadir == NULL)
     strcpy (tmp_filename, "/data/local/tmp/gnat-XXXXXX");
-  else 
+  else
     sprintf (tmp_filename, "%s/local/tmp/gnat-XXXXXX", datadir);
 
-  testfd = mkstemp (tmp_filename);  
+  testfd = mkstemp (tmp_filename);
   if (testfd != -1)
     {
       close (testfd);
@@ -1229,17 +1229,17 @@ __gnat_tmp_name (char *tmp_filename)
   char *sdcard = getenv ("EXTERNAL_STORAGE");
 
   if (sdcard == NULL)
-    strcpy (tmp_filename, "/sdcard/gnat-XXXXXX");      
+    strcpy (tmp_filename, "/sdcard/gnat-XXXXXX");
   else
     sprintf (tmp_filename, "%s/gnat-XXXXXX", sdcard);
-    
-  testfd = mkstemp (tmp_filename);  
+
+  testfd = mkstemp (tmp_filename);
   if (testfd != -1)
     {
       close (testfd);
       return;
     }
-       
+
   tmpnam (tmp_filename);
 
 #elif defined (linux) || defined (__FreeBSD__) || defined (__NetBSD__) \
@@ -3474,42 +3474,40 @@ _flush_cache()
 }
 #endif
 
-#if defined (IS_CROSS)  \
-  || (! ((defined (sparc) || defined (i386)) && defined (sun) \
-      && defined (__SVR4)) \
-      && ! (defined (linux) && (defined (i386) || defined (__x86_64__))) \
-      && ! (defined (linux) && defined (__ia64__)) \
-      && ! (defined (linux) && defined (powerpc)) \
-      && ! defined (__Lynx__) \
-      && ! defined (__hpux__) \
-      && ! defined (__APPLE__) \
-      && ! defined (_AIX) \
-      && ! (defined (__alpha__)  && defined (__osf__)) \
-      && ! defined (VMS) \
-      && ! defined (__MINGW32__) \
-      && ! (defined (__mips) && defined (__sgi)))
+/* run-time symbolic traceback support */
+#if defined (__DragonFly__) \
+ || defined (__FreeBSD__) \
+ || defined (__OpenBSD__) \
+ || defined (__NetBSD__)
 
-/* Dummy function to satisfy g-trasym.o. See the preprocessor conditional
-   just above for a list of native platforms that provide a non-dummy
-   version of this procedure in libaddr2line.a.  */
+/* The above platforms use the external program /usr/bin/addr2line */
+#define EXTERNAL_SYMTRACE
 
-void
-convert_addresses (const char *file_name ATTRIBUTE_UNUSED,
-		   void *addrs ATTRIBUTE_UNUSED,
-		   int n_addr ATTRIBUTE_UNUSED,
-		   void *buf ATTRIBUTE_UNUSED,
-		   int *len ATTRIBUTE_UNUSED)
-{
-  *len = 0;
-}
-#elif defined(__DragonFly__) \
-   || defined(__FreeBSD__) \
-   || defined(__OpenBSD__) \
-   || defined(__NetBSD__)
-   
+#elif defined (VMS) \
+   || defined (_AIX) \
+   || defined (__Lynx__) \
+   || defined (__hpux__)  \
+   || defined (__APPLE__) \
+   || defined (__MINGW32__) \
+   || (defined (__mips)    && defined (__sgi)) \
+   || (defined (__alpha__) && defined (__osf__)) \
+   || (defined (linux)     && defined (i386)) \
+   || (defined (linux)     && defined (powerpc)) \
+   || (defined (linux)     && defined (__ia64__)) \
+   || (defined (linux)     && defined (__x86_64__)) \
+   || (defined (__SVR4)    && defined (sun) && defined (i386)) \
+   || (defined (__SVR4)    && defined (sun) && defined (sparc))
+
+/* The above platforms use the system library libaddr2line.a */
+#define NATIVE_SYMTRACE
+#endif
+
+#if defined (EXTERNAL_SYMTRACE) && !defined (IS_CROSS)
+
 /*
   Copyright (C) 1999 by Juergen Pfeifer <juergen.pfeifer@gmx.net>
   Ada for Linux Team (ALT)
+  Heavily modified by John Marino <http://www.dragonlace.net>
 
   Permission is hereby granted, free of charge, to any person obtaining a
   copy of this software and associated documentation files (the
@@ -3537,124 +3535,156 @@ convert_addresses (const char *file_name ATTRIBUTE_UNUSED,
 */
 
 #include <sys/types.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
 
-#define MAX_LINE 1024
-
-#define CLOSE1  close(fd1[0]); close(fd1[1])
-#define CLOSE2  close(fd2[0]); close(fd2[1])
+#define CLOSE_SENDPIPE close(sendpipe[0]); close(sendpipe[1])
+#define CLOSE_READPIPE close(readpipe[0]); close(readpipe[1])
+#define DUP2CLOSE(oldfd, newfd) dup2(oldfd, newfd); close(oldfd);
 #define RESTSIG sigaction(SIGPIPE,&oact,NULL)
 
-void convert_addresses (const char *file_name,
-                void* addrs[],
-                int   n_addr,
-                char* buf,
-                int*  len)
+#define MAX_LINE     1024
+#define PARENT_READ  readpipe[0]
+#define CHILD_WRITE  readpipe[1]
+#define CHILD_READ   sendpipe[0]
+#define PARENT_WRITE sendpipe[1]
+
+void
+convert_addresses (const char *file_name,
+		   void *addrs,
+		   int   n_addr,
+		   void *buf,
+		   int  *len)
 {
   int max_len = *len;
-  pid_t pid = getpid();
-  pid_t child;
+  pid_t childpid;
 
   struct sigaction act, oact;
 
-  int fd1[2], fd2[2];
+  int sendpipe[2] = {-1,-1},	/* parent -> child */
+      readpipe[2] = {-1,-1};	/* parent <- child */
 
-  *buf = 0; *len = 0;
+  *len = 0;
   act.sa_handler = SIG_IGN;
   sigemptyset(&act.sa_mask);
   act.sa_flags = 0;
   if (sigaction(SIGPIPE,&act,&oact) < 0)
     return;
 
-  if (pipe(fd1) >= 0) {                                   /* cond ALPHA */
-    if (pipe(fd2)>=0) {                                   /* cond BRAVO */
-      if ((child = fork()) < 0) {                         /* cond CHARLIE */
-        CLOSE1; CLOSE2; RESTSIG;
-        return;
+  if (pipe(sendpipe) < 0) { RESTSIG; return; }
+  if (pipe(readpipe) < 0) { CLOSE_SENDPIPE; RESTSIG; return; }
+  if ((childpid = fork()) < 0) {
+    CLOSE_READPIPE;
+    CLOSE_SENDPIPE;
+    RESTSIG;
+    return;
+  }
+
+  if (childpid == 0) {    /* child process */
+    close(PARENT_WRITE);
+    close(PARENT_READ);
+    if ((CHILD_READ != STDIN_FILENO) && (CHILD_WRITE != STDOUT_FILENO)) {
+      if ((CHILD_READ == STDOUT_FILENO) && (CHILD_WRITE == STDIN_FILENO)) {
+        const int temp_fd = dup(CHILD_WRITE);
+        close (CHILD_WRITE);
+        DUP2CLOSE (CHILD_READ, STDIN_FILENO);
+        DUP2CLOSE (temp_fd,    STDOUT_FILENO);
       }
-      else {                                              /* else CHARLIE */
-        if (0==child) {                                   /* cond DELTA */
-          close(fd1[1]);
-          close(fd2[0]);
-          if (fd1[0] != STDIN_FILENO) {
-            if (dup2(fd1[0],STDIN_FILENO) != STDIN_FILENO) {
-              CLOSE1; CLOSE2;
-            }
-            close(fd1[0]);
-          }
-          if (fd2[1] != STDOUT_FILENO) {
-            if (dup2(fd2[1],STDOUT_FILENO) != STDOUT_FILENO) {
-              CLOSE1; CLOSE2;
-            }
-            close(fd2[1]);
-          }
-
-          /* As pointed out by Florian Weimer to JP, it is a
-             security threat to call the script with a user defined
-             environment and using the path. That would be Trojans
-             pleasure.  Therefore the absolute path to addr2line and
-             an empty environment is used. That should be safe.
-          */
-          char *const argv[] = { "addr2line",
-                                 "-e", file_name,
-                                 "--demangle=gnat",
-                                 "--functions",
-                                 "--basenames",
-                                 NULL };
-          char *const envp[] = { NULL };
-          if (execve("/usr/bin/addr2line", argv, envp) < 0) {
-            CLOSE1; CLOSE2;
-          }
-        }
-        else {                                            /* else DELTA */
-          int i, n;
-          char hex[16];
-          char line[MAX_LINE + 1];
-          char *p;
-          char *s = buf;
-
-          /* Parent context */
-          close(fd1[0]);
-          close(fd2[1]);
-
-          for(i=0; i < n_addr; i++) {
-            snprintf(hex,sizeof(hex),"%p\n",addrs[i]);
-            write(fd1[1],hex,strlen(hex));
-            n = read(fd2[0],line,MAX_LINE);
-            if (n<=0)
-              break;
-            line[n]=0;
-            /* We have approx. 16 additional chars for "%p in " clause.
-               We use this info to prevent a buffer overrun.
-            */
-            if (n + 16 + (*len) > max_len)
-              break;
-            p = strchr(line,'\n');
-            if (p) {
-              if (*(p+1)) {
-                *p = 0;
-                *len += snprintf(s, (max_len - (*len)), "%p in %s at %s",
-                                 addrs[i], line, p+1);
-              }
-              else {
-                *len += snprintf(s, (max_len - (*len)), "%p at %s",
-                                 addrs[i], line);
-              }
-              s = buf + (*len);
-            }
-          }
-          close(fd1[1]);
-          close(fd2[0]);
-        }                                                 /* ends DELTA */
-      }                                                   /* ends CHARLIE */
+      else if ((CHILD_READ == STDIN_FILENO) && (CHILD_WRITE > 1)) {
+        DUP2CLOSE (CHILD_WRITE, STDOUT_FILENO);
+      }
+      else if ((CHILD_READ > 1) && (CHILD_WRITE == STDOUT_FILENO)) {
+        DUP2CLOSE (CHILD_READ, STDIN_FILENO);
+      }
+      else if ((CHILD_READ > 1) && (CHILD_WRITE == STDIN_FILENO)) {
+        DUP2CLOSE (CHILD_WRITE, STDOUT_FILENO);
+        DUP2CLOSE (CHILD_READ,  STDIN_FILENO);
+      }
+      else {
+        /* CHILD_READ >= 1 and CHILD_WRITE > 1 */
+        DUP2CLOSE (CHILD_READ,  STDIN_FILENO);
+        DUP2CLOSE (CHILD_WRITE, STDOUT_FILENO);
+      }
     }
-    else {                                                /* else BRAVO */
-      CLOSE1;
-    }                                                     /* ends BRAVO */
-  }                                                       /* ends ALPHA */
+    /* As pointed out by Florian Weimer to JP, it is a security threat to call
+       the script with a user defined environment and using the path. That
+       would be Trojans pleasure.  Therefore the absolute path to addr2line
+       and an empty environment is used. That should be safe.
+    */
+    char *const argv[] = { "addr2line",
+                           "-e", file_name,
+                           "--demangle=gnat",
+                           "--functions",
+                           "--basenames",
+                           NULL };
+    char *const envp[] = { NULL };
+    if (execve("/usr/bin/addr2line", argv, envp) < 0) {
+      close (CHILD_WRITE);
+      close (CHILD_READ);
+      RESTSIG;
+      exit (1);
+    }
+  }
+
+  /* Below this line is parent process */
+  int i, n;
+  char hex[16];
+  char line[MAX_LINE + 1];
+  char *p;
+  char *s = buf;
+  long *trace_address = addrs;
+
+  close(CHILD_WRITE);
+  close(CHILD_READ);
+
+  for(i=0; i < n_addr; i++) {
+    snprintf(hex,sizeof(hex),"%#lx\n",*trace_address);
+    write(PARENT_WRITE,hex,strlen(hex));
+    n = read(PARENT_READ,line,MAX_LINE);
+    if (n<=0)
+      break;
+
+    line[n]=0;
+    /* We have approx. 16 additional chars for "%#lx in " clause.
+       We use this info to prevent a buffer overrun. */
+    if (n + 16 + (*len) > max_len)
+      break;
+
+    p = strchr(line,'\n');
+    if (p) {
+      if (*(p+1)) {
+        *p = 0;
+        *len += snprintf(s, (max_len - (*len)), "%#lx in %s at %s",
+                         *trace_address, line, p+1);
+      }
+      else {
+        *len += snprintf(s, (max_len - (*len)), "%#lx at %s",
+                         *trace_address, line);
+      }
+      s = buf + (*len);
+    }
+    trace_address += 1;
+  }
+  close (PARENT_WRITE);
+  close (PARENT_READ);
   RESTSIG;
+}
+
+#elif defined (IS_CROSS) || !defined (NATIVE_SYMTRACE)
+
+/* run-time symbolic traceback support
+   Dummy function to satisfy g-trasym.o. */
+
+void
+convert_addresses (const char *file_name ATTRIBUTE_UNUSED,
+		   void *addrs ATTRIBUTE_UNUSED,
+		   int n_addr ATTRIBUTE_UNUSED,
+		   void *buf ATTRIBUTE_UNUSED,
+		   int *len ATTRIBUTE_UNUSED)
+{
+  *len = 0;
 }
 #endif
 
