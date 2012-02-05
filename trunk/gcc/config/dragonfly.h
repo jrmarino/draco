e7013ec3ddcb8dca4bf2e87338a45cf80333f901
John Marino <aux@marino.st>
2012-02-05 10:40:38 -0600

    Remove !static restriction for error handling on DragonFly
    Since DragonFly 3.0, it can handle exceptions on statically-built
    binaries.

--- trunk/gcc/config/dragonfly.h
+++ trunk/gcc/config/dragonfly.h
@@ -95,12 +95,10 @@ along with GCC; see the file COPYING3.  If not see
 #undef  LINK_SPEC
 #define LINK_SPEC DFBSD_LINK_SPEC
 
-#define	DFBSD_DYNAMIC_LINKER \
-  "/usr/libexec/ld-elf.so.2"
+#define	DFBSD_DYNAMIC_LINKER "/usr/libexec/ld-elf.so.2"
 
 #if defined(HAVE_LD_EH_FRAME_HDR)
-#define LINK_EH_SPEC \
-  "%{!static:--eh-frame-hdr}"
+#define LINK_EH_SPEC "--eh-frame-hdr"
 #endif
 
 /* Use --as-needed -lgcc_s for eh support.  */
