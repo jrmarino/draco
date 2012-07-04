ce771ec9a3ff0e71ea6c2db6b0efe876dae19e1d
John Marino <aux@marino.st>
2012-07-04 06:22:42 +0200

    Upgrade GPRBUILD to version GPL 2012
    -- Adds the -R option
    -- completely untested.

--- misc/package_source/pkgsrc/devel/gprbuild-aux/buildlink3.mk
+++ misc/package_source/pkgsrc/devel/gprbuild-aux/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	gprbuild-aux
 .if !defined(GPRBUILD_AUX_BUILDLINK3_MK)
 GPRBUILD_AUX_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.gprbuild-aux+=	gprbuild-aux>=20101120
+BUILDLINK_API_DEPENDS.gprbuild-aux+=	gprbuild-aux>=20120510
 BUILDLINK_PKGSRCDIR.gprbuild-aux?=	../../devel/gprbuild-aux
 
 .include "../../textproc/xmlada/buildlink3.mk"
