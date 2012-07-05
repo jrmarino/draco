55242f83c93c25a1f0341bf9f9f78b029c8e1827
John Marino <aux@marino.st>
2012-07-06 00:56:51 +0200

    Update gtkada-2.22 for use with gcc-aux

--- misc/package_source/pkgsrc/x11/gtkada/buildlink3.mk
+++ misc/package_source/pkgsrc/x11/gtkada/buildlink3.mk
@@ -10,7 +10,6 @@ BUILDLINK_ABI_DEPENDS.gtkada+=	gtkada>=2.22nb4
 BUILDLINK_PKGSRCDIR.gtkada?=	../../x11/gtkada
 
 .include "../../x11/gtk2/buildlink3.mk"
-.include "../../lang/gnat-aux/buildlink3.mk"
 .endif	# GTKADA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-gtkada
