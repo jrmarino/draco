43ab430654e1fdb794eda3345a5f10944892e527
John Marino <aux@marino.st>
2012-07-05 17:46:06 +0200

    Fix www/aws!
    1) use lang/gcc-aux via USE_LANGAGES+= ada
    2) fix rpath with WRKOBJDIR paths issue

--- misc/package_source/pkgsrc/www/aws/options.mk
+++ misc/package_source/pkgsrc/www/aws/options.mk
@@ -16,9 +16,11 @@ PKG_SUGGESTED_OPTIONS=	ssl
 .include "../../mk/bsd.prefs.mk"
 .include "../../mk/bsd.options.mk"
 
-CONFIGURE_ARGS+=	GCC=gnatgcc
+CONFIGURE_ARGS+=	GCC=ada
 CONFIGURE_ARGS+=	PROCESSORS=1
 CONFIGURE_ARGS+=	PYTHON=python${PYVERSSUFFIX}
+CONFIGURE_ARGS+=	XMLADA=true
+CONFIGURE_ARGS+=	prefix=${PREFIX}
 DOTBUILD=		release
 
 # The system libgcc is from version 4.1 which doesn't include the symbol
@@ -33,6 +35,8 @@ DOTBUILD=		release
 
 .if $(OPSYS) == "NetBSD"
 CONFIGURE_ARGS+=	ZLIB=false
+.else
+CONFIGURE_ARGS+=	ZLIB=true
 .endif
 
 ###################
