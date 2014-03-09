b2a5bb3fa513da1061409bb2fa5c218cd29f886f
John Marino <draco@marino.st>
2014-03-09 23:42:41 +0100

    Sync thin socket bindings -- not sure what happened here

--- v4.9/gcc/ada/g-socthi-netbsd.adb
+++ v4.9/gcc/ada/g-socthi-netbsd.adb
@@ -6,7 +6,7 @@
 --                                                                          --
 --                                 B o d y                                  --
 --                                                                          --
---                     Copyright (C) 2001-2012, AdaCore                     --
+--                     Copyright (C) 2001-2013, AdaCore                     --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -45,9 +45,6 @@ with GNAT.OS_Lib; use GNAT.OS_Lib;
 with Interfaces.C; use Interfaces.C;
 
 package body GNAT.Sockets.Thin is
-
-   Unknown_System_Error : constant C.Strings.chars_ptr :=
-                            C.Strings.New_String ("Unknown system error");
 
    function Syscall_Accept
      (S       : C.int;
@@ -356,8 +353,6 @@ package body GNAT.Sockets.Thin is
    -- Socket_Error_Message --
    --------------------------
 
-   function Socket_Error_Message
-     (Errno : Integer) return C.Strings.chars_ptr
-   is separate;
+   function Socket_Error_Message (Errno : Integer) return String is separate;
 
 end GNAT.Sockets.Thin;
