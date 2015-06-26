2630bc96152ecbec08a39067ea054594bce857c9
John Marino <draco@marino.st>
2015-06-26 09:16:17 +0200

    Start private ports using "adacore" as category

---
+++ misc/gpl-ports/Mk/Uses/adacore.mk
@@ -0,0 +1,27 @@
+# $FreeBSD$
+#
+# Establish GNAT GPL 20XX compiler as a build dependency
+# To change default compiler, define ADA_DEFAULT in make.conf to 5
+#
+# Feature:      adacore
+# Usage:        USES=ada
+# Valid ARGS:   run
+#
+# MAINTAINER: marino@FreeBSD.org
+
+.if !defined(_INCLUDE_USES_ADACORE_MK)
+_INCLUDE_USES_ADACORE_MK=    yes
+
+CC= ada
+
+. if ${ada_ARGS:Mrun}
+RUN_DEPENDS+=	${LOCALBASE}/gnat-gpl/bin/ada:${PORTSDIR}/adacore/gnat-gpl
+. endif
+
+BUILD_DEPENDS+=	${LOCALBASE}/gnat-gpl/bin/ada:${PORTSDIR}/adacore/gnat-gpl
+MAKE_ENV+=	PATH=${LOCALBASE}/gnat-gpl/bin:${PATH} \
+		ADA_PROJECT_PATH=${LOCALBASE}/lib/gnat-gpl
+CONFIGURE_ENV+=	PATH=${LOCALBASE}/gnat-gpl/bin:${PATH} \
+		ADA_PROJECT_PATH=${LOCALBASE}/lib/gnat-gpl
+
+.endif
