0fa1c408a4b6873df24f72fdf5cebba6b934e66b
John Marino <draco@marino.st>
2018-01-18 10:13:29 -0600

    Include system library patch in solaris (avoids rpath-link?)

--- v7/gcc/config/sol2.h
+++ v7/gcc/config/sol2.h
@@ -350,7 +350,7 @@ along with GCC; see the file COPYING3.  If not see
 #undef  LINK_SPEC
 #define LINK_SPEC \
   "%{h*} %{v:-V} \
-   %{!shared:%{!static:-R@PREFIX@/@GCCAUX@/lib/amd64 %{rdynamic: " RDYNAMIC_SPEC "}}} \
+   %{!shared:%{!static:-R/lib/amd64:@PREFIX@/@GCCAUX@/lib/amd64 %{rdynamic: " RDYNAMIC_SPEC "}}} \
    %{static:-dn -Bstatic} \
    %{shared:-G -dy %{!mimpure-text:-z text}} " \
    LINK_LIBGCC_MAPFILE_SPEC LINK_CLEARCAP_SPEC " \
