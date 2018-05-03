5308c87b25e4339cf3c634270a652067577b6f51
John Marino <draco@marino.st>
2018-05-03 11:52:43 -0500

    First iteration of gcc8 support

---
+++ misc/flux8/patch-gcc_config_i386_gnu-user64.h
@@ -0,0 +1,10 @@
+--- gcc/config/i386/gnu-user64.h.orig	2018-05-03 15:12:22 UTC
++++ gcc/config/i386/gnu-user64.h
+@@ -57,6 +57,7 @@ see the files COPYING3 and COPYING.RUNTI
+                    %{" SPEC_32 ":-m " GNU_USER_LINK_EMULATION32 "} \
+                    %{" SPEC_X32 ":-m " GNU_USER_LINK_EMULATIONX32 "} \
+   %{shared:-shared} \
++  %{!static:--enable-new-dtags -rpath @PREFIX@/@GCCAUX@/lib64} \
+   %{!shared: \
+     %{!static: \
+       %{!static-pie: \
