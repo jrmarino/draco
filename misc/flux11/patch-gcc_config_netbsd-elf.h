84118d370b45b61d14888e62f7794919649f6840
John Marino <draco@marino.st>
2021-10-14 00:04:38 -0500

    support stack-checking on netbsd

--- misc/flux11/patch-gcc_config_netbsd-elf.h
+++ misc/flux11/patch-gcc_config_netbsd-elf.h
@@ -1,6 +1,6 @@
---- gcc/config/netbsd-elf.h.orig	2021-07-28 06:55:07.112284902 +0000
-+++ gcc/config/netbsd-elf.h	2021-10-03 13:44:36.569483651 +0000
-@@ -80,6 +80,7 @@
+--- gcc/config/netbsd-elf.h.orig	2021-10-14 01:03:51 UTC
++++ gcc/config/netbsd-elf.h
+@@ -80,6 +80,7 @@ along with GCC; see the file COPYING3.
    "%{assert*} %{R*} %{rpath*} \
     %{shared:-shared} \
     %{symbolic:-Bsymbolic} \
@@ -8,3 +8,13 @@
     %{!shared: \
       -dc -dp \
       %{!nostdlib: \
+@@ -101,6 +102,9 @@ along with GCC; see the file COPYING3.
+ #undef SUBTARGET_EXTRA_SPECS
+ #define SUBTARGET_EXTRA_SPECS   NETBSD_SUBTARGET_EXTRA_SPECS
+ 
++/* Define this to be nonzero if static stack checking is supported. */
++#define STACK_CHECK_STATIC_BUILTIN 1
++
+ /* Use --as-needed -lgcc_s for eh support.  */
+ #ifdef HAVE_LD_AS_NEEDED
+ #define USE_LD_AS_NEEDED 1
