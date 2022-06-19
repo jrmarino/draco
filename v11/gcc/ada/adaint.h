009d4f3369a3c02b8485a03ee20b574cd8a36dc5
John Marino <draco@marino.st>
2022-06-19 10:02:34 -0500

    add in patch from ravenports gcc11

--- v11/gcc/ada/adaint.h
+++ v11/gcc/ada/adaint.h
@@ -365,6 +365,7 @@ extern int    __gnat_sigaltstack (const stack_t *, stack_t *);
 extern int    __gnat_sigaction   (int, const struct sigaction *, struct sigaction *);
 #endif
 #include <time.h>
+#include <sys/time.h>
 extern int    __gnat_nanosleep     (const struct timespec *, struct timespec *);
 extern int    __gnat_gettimeofday  (struct timeval *, struct timezone *);
 #include <sys/select.h>
